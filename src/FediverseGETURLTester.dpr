0935b7bccd5d59cca7f5f58bd51f112bf540e0f3
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-03-18 19:08:37 +0100

    replaced private dependency to AboutBox dialog component by the public GitHub project (#4)

--- src/FediverseGETURLTester.dpr
+++ src/FediverseGETURLTester.dpr
@@ -5,8 +5,8 @@ uses
   FMX.Forms,
   fMain in 'fMain.pas' {frmMain},
   u_urlOpen in '..\lib-externe\librairies\u_urlOpen.pas',
-  Olf.FMX.AboutDialog in '..\lib-externe\BoiteDeDialogueAPropos\sources\Olf.FMX.AboutDialog.pas',
-  Olf.FMX.AboutDialogForm in '..\lib-externe\BoiteDeDialogueAPropos\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
+  Olf.FMX.AboutDialog in '..\lib-externe\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
+  Olf.FMX.AboutDialogForm in '..\lib-externe\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
   Olf.RTL.Params in '..\lib-externe\librairies\Olf.RTL.Params.pas',
   uChecksumVerif in '..\lib-externe\librairies\uChecksumVerif.pas',
   u_md5 in '..\lib-externe\librairies\u_md5.pas';
